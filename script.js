3f7db6b70403e98912a1ea73e02c5abb01f5e98b
Francisco <francisco.s.student@lyonscommunityschool.org>
2017-02-15 15:06:01 +0000

    buildhouse

---
+++ script.js
@@ -0,0 +1,16 @@
+    TinyTurtle.apply(window);
+    
+    right(90);
+    forward(60)
+    left(90)
+    forward(40)
+    left(90)
+    forward(60)
+    left(90)
+    forward(40)
+    right(180)
+    forward(40)
+    right(45)
+    forward(40)
+    right(90)
+    forward(40)
